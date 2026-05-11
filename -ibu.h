1f3ec78510e89384e8cda589949a41c7a88dc193
fairus0305 <ruszezzy@gmail.com>
2026-05-11 14:54:41 +0700

    membuat distructor ibu

--- -ibu.h
+++ -ibu.h
@@ -7,7 +7,9 @@ public :
     string nama;
     vector<anak *> daftar_anak;
 
-    ibu(string pNama) : nama(pNama)
-    {
+    ibu(string pNama) : nama(pNama){
         cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu(){
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
